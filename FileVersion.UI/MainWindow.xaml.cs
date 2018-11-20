74284716620c20e3f92d0fe83124875d98cad889
meishuai <1823852132@qq.com>
2018-11-20 17:27:26 +0800

    添加项目文件。

---
+++ FileVersion.UI/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileVersion.UI
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FileHashProvider p1 = new FileHashProvider(this.Check1.IsChecked == true, this.TbQueryTmp.Text);
+            var list = p1.GetDirInfo("./");
+            UpdateDirInfo(list);
+        }
+
+
+
+        public void UpdateDirInfo(List<FileHashInfo> list)
+        {
+            DataGrid1.ItemsSource = list;
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.BtnGet.IsEnabled = false;
+            var isQuery= this.Check1.IsChecked == true;
+            var queryTmp = this.TbQueryTmp.Text;
+            var path = this.TbFile.Text;
+            var task = Task.Run(() =>
+            {
+                FileHashProvider p1 = new FileHashProvider(isQuery,queryTmp );
+                var list = p1.GetDirInfo(path);
+                var item = p1.GetFileHashInfo(path);
+                if (item != null)
+                {
+                    list.Insert(0, item);
+                }
+                this.Dispatcher.InvokeAsync(()=>UpdateDirInfo(list));
+            });
+            task.ContinueWith((t) =>
+            {
+                Dispatcher.Invoke(() => this.BtnGet.IsEnabled = true);
+                return t;
+            });
+        }
+
+        private void DockPanel_Drop(object sender, DragEventArgs e)
+        {
+            string[] files=null;
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            }
+
+            this.BtnGet.IsEnabled = false;
+            var isQuery = this.Check1.IsChecked == true;
+            var queryTmp = this.TbQueryTmp.Text;
+            var task = Task.Run(() =>
+            {
+                FileHashProvider p1 = new FileHashProvider(isQuery, queryTmp);
+                var list = new List<FileHashInfo>();
+                foreach (var path in files)
+                {
+                    var item = p1.GetFileHashInfo(path);
+                    list.Add(item);
+                }
+                
+                this.Dispatcher.InvokeAsync(() => UpdateDirInfo(list));
+            });
+            task.ContinueWith((t) =>
+            {
+                Dispatcher.Invoke(() => this.BtnGet.IsEnabled = true);
+                return t;
+            });
+        }
+    }
+}
---
+++ FileVersion/FileHashProvider.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileVersion
+{
+    public class FileHashProvider
+    {
+        private bool _isQueryTmp=true;
+        private string _queryTmp="?v={0}"; 
+
+        public FileHashProvider()
+        {
+
+        }
+
+        public FileHashProvider(bool isQueryTmp,string queryTmp)
+        {
+            this._isQueryTmp = isQueryTmp;
+            _queryTmp =queryTmp;
+        }
+
+        SHA256 c1 = SHA256.Create();
+
+        public string Get(string path)
+        {
+            var hashBuffer = c1.ComputeHash(File.ReadAllBytes(path));
+            var result = Convert.ToBase64String(hashBuffer);
+            return FilterStr(result.Replace('+', '-').Replace('/', '_').Replace("=", string.Empty));
+        }
+
+        private string FilterStr(string str)
+        {
+            if (_isQueryTmp)
+            {
+                return string.Format(_queryTmp, str);
+            }
+            return str;
+        }
+
+        public List<FileHashInfo> GetDirInfo(string dir)
+        {
+            List<FileHashInfo> list = new List<FileHashInfo>();
+            if (!Directory.Exists(dir)) return list;
+            var files = Directory.GetFiles(dir);
+            foreach (var item in files)
+            {
+                list.Add(new FileHashInfo { Version = Get(item), FileName = item });
+            }
+            return list;
+        }
+
+        public FileHashInfo GetFileHashInfo(string path)
+        {
+            if (!File.Exists(path)) return null;
+           return new FileHashInfo { Version = Get(path), FileName = path };
+        }
+    }
+}
